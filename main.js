cdae143e40bca6e467477f961f9d523c71f1497d
noname306 <ya.sitora11@gmail.com>
2024-12-24 20:50:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const ism = prompt("string")
+const num = +prompt("raqam")
+const hayoq = prompt("boolean")
+
+alert(`${ism} ${typeof ism } malumot turiga kiradi
+    ${num} ${typeof num } malumot turiga kiradi
+    ${hayoq} ${typeof Boolean(hayoq) } malumot turiga kiradi`)
+
+;
